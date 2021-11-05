932181040b768f7d8bccc46e556e33453bc57d47
Liza Degtyareva <liza.degt@yandex.ru>
2021-11-05 23:04:20 +0300

    JUnit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,34 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldReturn1IfAmountIs999() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
 
+        int actual = service.remain(amount);
+        int expected = 1;
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldReturn0IfAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldReturn900IfAmountIs1100() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+
+        int actual = service.remain(amount);
+        int expected = 900;
+        assertEquals(expected,actual);
+    }
 }
